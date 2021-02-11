aa37b7989d49538e72695452436534ae22e013f5
Finn <finn1996@freenet.de>
2021-02-11 17:14:01 +0100

    added a few visual changes
    showing name of player in header +
    background contente is now the whjole page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PlayerService} from './services/player.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+
+  constructor(public playerService: PlayerService) {
+  }
 }
